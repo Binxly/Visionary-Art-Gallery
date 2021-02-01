7cd03a3e84c6d67e8d3b62da20c5cba59e380b20
Zac Bagley <zbagley610@gmail.com>
2021-01-31 22:19:29 -0500

    Visionary Gallery Pages and Folders

---
+++ scripts/myFunctions.js
@@ -0,0 +1,8 @@
+/* Zachery Bagley
+November 18th, 2019
+*/
+
+//Thank you alert
+function thankYou() {
+	alert("Thank you for your feedback. Please allow 24-48 hours for a response via the email provided.");
+}
